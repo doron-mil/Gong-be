040f95ef75f2f0db779072b4f883780d939ebef0
Doron - Webiks <doron_miller@webiks.com>
2018-08-23 15:33:29 +0300

    “master” - working boilerplate

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ db.connection.on('error', (err) => {
   logger.log('error', `connection error:${err}`);
 });
 
-db.connection.once('open', () => {
-  app.listen(PORT, (err) => {
-    if (err) {
-      logger.log('error', `Error starting server: ${err}`);
-      return;
-    }
-    logger.log('info', `server listening on ${PORT}`);
-  });
-});
+// db.connection.once('open', () => {
+//   app.listen(PORT, (err) => {
+//     if (err) {
+//       logger.log('error', `Error starting server: ${err}`);
+//       return;
+//     }
+//     logger.log('info', `server listening on ${PORT}`);
+//   });
+// });
