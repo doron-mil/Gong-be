7608424a8798200c9b3d5c508fbc0dacc2a546ea
doron <doron.miller@gmail.com>
2019-02-17 07:34:10 +0200

    master - adding babbel support while developing shortend schema for describing a course - for debugging

--- model/job.js
+++ model/job.js
@@ -1,6 +1,14 @@
 module.exports = class Job {
+  static statusEnum = Object.freeze({
+    SCHEDULED: Symbol('SCHEDULED'),
+    DONE: Symbol('DONE1'),
+    DELETED: Symbol('DELETED'),
+    FAILED: Symbol('FAILED'),
+  });
+
   constructor(aTime, aData) {
     this.data = aData;
     this.time = aTime;
+    this.status = Job.statusEnum.SCHEDULED;
   }
 };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const config = require('config');
+const moment = require('moment');
 const app = require('./app');
 const db = require('../lib/db');
 const logger = require('../lib/logger');
+
 const scheduleManager = require('../lib/scheduleManager');
-const manualGongsManager = require('../lib/gongsManager');
+const gongsManager = require('../lib/gongsManager');
 const relayAndSoundManager = require('../lib/relayAndSoundManager');
 
 const PORT = config.get('server.port');
@@ -18,15 +20,20 @@ db.connection.on('error', (err) => {
 });
 
 scheduleManager.start();
-scheduleManager.setExecuter(relayAndSoundManager.playGong);
-manualGongsManager.addOnAddGongListener(scheduleManager.addJob.bind(scheduleManager));
-scheduleManager.addJobsArray(manualGongsManager.getGongsAsJobsArray());
+scheduleManager.setExecutor(relayAndSoundManager.playGong);
+gongsManager.addOnAddGongListener(scheduleManager.addJob.bind(scheduleManager));
+scheduleManager.addJobsArray(gongsManager.getGongsAsJobsArray());
 
-const time = new Date().getTime() + 60000;
-scheduleManager.addJob({
-  time,
-  data: { stam: 1 },
-});
+// const time = new Date().getTime() + 60000;
+// scheduleManager.addJob({
+//   time,
+//   data: { stam: 1 },
+// });
+
+const aaaa = scheduleManager.getNextScheduledJob();
+console.log('11111', moment(aaaa.time)
+  .format('YY-MM-DD HH:mm:ss'), aaaa.data);
+
 // scheduleManager.start();
 
 // db.connection.once('open', () => {
