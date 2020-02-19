65844f4efed5849c43fd1c9adff06f7af077eb33
doron <doron.miller@gmail.com>
2020-02-20 00:36:33 +0100

    Docker Dep #3

---
+++ dev_ops/deploy_gong.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#echo "$#"
+#echo "$1"
+
+mkdir -p /home/dhamma/projects/gong_dev_ops/dev_ops_logs
+new_log_file=/home/dhamma/projects/gong_dev_ops/dev_ops_logs/dev_ops_log_$(date +"%Y_%m_%d_%H_%M_%S")_dev.log
+
+USER=$(whoami)
+mkdir -p /home/${USER}/projects/
+mkdir -p /home/${USER}/projects/gong_dev_ops
+cd /home/${USER}/projects/
+
+git clone https://github.com/doron-mil/Gong-be.git
+git clone https://github.com/doron-mil/Gong_fe.git
+
+cp -rf /home/${USER}/projects/Gong-be/dev_ops /home/${USER}/projects/gong_dev_ops/
+cp -f /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_dev_ops.sh /home/${USER}/projects/gong_dev_ops/
+cp -f /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_gong_server.sh /home/${USER}/projects/gong_dev_ops/
+
+
+export HISTIGNORE='*sudo -S*'
+set +v
+sudo -S whoami <<< $1
