6f7b333158908569f5ff19ff694751761731d2c0
doron <doron.miller@gmail.com>
2020-02-20 21:01:12 +0100

    Docker Dep #4

--- dev_ops/deploy_gong.sh
+++ dev_ops/deploy_gong.sh
@@ -18,7 +18,7 @@ cp -rf /home/${USER}/projects/Gong-be/dev_ops /home/${USER}/projects/gong_dev_op
 cp -f /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_dev_ops.sh /home/${USER}/projects/gong_dev_ops/
 cp -f /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_gong_server.sh /home/${USER}/projects/gong_dev_ops/
 
-
 export HISTIGNORE='*sudo -S*'
-set +v
-sudo -S whoami <<< $1
+sudo -S /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_gong_server_be.sh <<< $1
+sudo -S /home/${USER}/projects/gong_dev_ops/dev_ops/refresh_gong_server_fe.sh <<< $1
+
